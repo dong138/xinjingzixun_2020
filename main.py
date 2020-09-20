8195e075b39ba337a38108730c227acd1bc8af5e
dong4716138 <dong4716138@163.com>
2020-09-20 21:11:55 +0800

    13-flask对象加载配置文件信息

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from flask import Flask
 
 from views import index_blu
+from models import db
 
 # 创建flask应用对象
 app = Flask(__name__)
+
+# 加载配置
+app.config.from_pyfile("config.ini")
 
 # 创建蓝图，且注册到app
 app.register_blueprint(index_blu)
