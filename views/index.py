2ef2431c29862f99b3c3bbe7b28a291942e9c41e
dong4716138 <dong4716138@163.com>
2020-09-20 21:39:26 +0800

    18-通过SQLAlchemy查询了6条点击量最高的信息，然后通过render_template传递到index.html文件中，用JinJa2语法循环从而得到了循环6次，验证了从数据库中获取数据的成功

--- views/index.py
+++ views/index.py
@@ -1,8 +1,13 @@
 from flask import render_template
 
+from models import db
+from models.index import News
+
 from . import index_blu
 
 
 @index_blu.route("/")
 def index():
-    return render_template("index.html")
+    # 查询点击量最多的前6个新闻信息
+    clicks_top_6_news = db.session.query(News).order_by(-News.clicks).limit(6)
+    return render_template("index.html", clicks_top_6_news=clicks_top_6_news)
