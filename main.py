0d018ad330418ce4b474bfd6e46eb7c6fa20fe38
dong4716138 <dong4716138@163.com>
2020-09-20 21:48:55 +0800

    22-前3个新闻序号用不同的样式展示（方式2：过滤器）；新建立utils包，这里存储公共的工具或者第三方的工具例如发送短信验证码

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from flask_script import Manager
 
 from views import index_blu
 from models import db
+from utils.common import show_top_6_news_style
 
 # 创建flask应用对象
 app = Flask(__name__)
@@ -16,6 +17,9 @@ app.register_blueprint(index_blu)
 
 # 初始化数据库
 db.init_app(app)
+
+# 添加过滤器
+app.add_template_filter(show_top_6_news_style)
 
 # 添加数据库迁移等工具
 manager = Manager(app)
---
+++ utils/common.py
@@ -0,0 +1,8 @@
+def show_top_6_news_style(index):
+    if index == 1:
+        return "first"
+    elif index == 2:
+        return "second"
+    elif index == 3:
+        return "third"
+    return ""
