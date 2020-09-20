8b253f369622da508ef0496c4e7f7871e065ae30
dong4716138 <dong4716138@163.com>
2020-09-20 21:06:55 +0800

    11-定义News、Category模型类（News表示新闻，Category表示分类， 他们之间是多对一，New多 Cate一）

--- models/index.py
+++ models/index.py
@@ -1,1 +1,33 @@
+from datetime import datetime
+
 from . import db
+
+
+class News(db.Model):
+    """新闻"""
+    __tablename__ = "news"
+
+    id = db.Column(db.Integer, primary_key=True)  # 新闻编号
+    title = db.Column(db.String(256), nullable=False)  # 新闻标题
+    source = db.Column(db.String(64), nullable=False)  # 新闻来源
+    digest = db.Column(db.String(512), nullable=False)  # 新闻摘要
+    content = db.Column(db.Text, nullable=False)  # 新闻内容
+    clicks = db.Column(db.Integer, default=0)  # 浏览量
+    index_image_url = db.Column(db.String(256))  # 新闻列表图片路径
+    status = db.Column(db.Integer, default=0)  # 当前新闻状态 如果为0代表审核通过，1代表审核中，-1代表审核不通过
+    reason = db.Column(db.String(256))  # 未通过原因，status = -1 的时候使用
+    create_time = db.Column(db.DateTime, default=datetime.now)  # 记录的创建时间
+    update_time = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)  # 记录的更新时间
+    category_id = db.Column(db.Integer, db.ForeignKey("category.id"))
+
+    category = db.relationship('Category', backref='news')
+
+
+class Category(db.Model):
+    """新闻分类"""
+    __tablename__ = "category"
+
+    id = db.Column(db.Integer, primary_key=True)  # 分类编号
+    name = db.Column(db.String(64), nullable=False)  # 分类名
+    create_time = db.Column(db.DateTime, default=datetime.now)  # 记录的创建时间
+    update_time = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)  # 记录的更新时间
