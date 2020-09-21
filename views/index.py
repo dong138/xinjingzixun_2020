f7c6ffd3e25d2104cd749d15645d1c9f2bec4ecc
dong4716138 <dong4716138@163.com>
2020-09-21 21:50:54 +0800

    30-按照分类显示正确的新闻列表

--- views/index.py
+++ views/index.py
@@ -19,11 +19,16 @@ def category_news():
     # http://127.0.0.1:8899/newslist?page=1&cid=1&per_page=10
     # 要从上述URL中提取page、cid、per_page的值
     page = request.args.get('page', 1)  # 前端要的是哪一页的数据
-    cid = request.args.get('cid', 0)  # 前端要的是哪个分类的数据，是股市、债市还是商品、外汇、公司
+    cid = int(request.args.get('cid', 0))  # 前端要的是哪个分类的数据，是股市、债市还是商品、外汇、公司
     per_page = request.args.get('per_page', 1)  # 前端要的是每一页中的新闻个数
 
     # 2. 到数据库中查询数据
-    paginate = db.session.query(News).paginate(page=int(page), per_page=int(per_page), error_out=False)
+    # 如果cid是0，表示要看最新的，如果不是0则按照原来规则查询
+    if cid == 0:
+        paginate = db.session.query(News).order_by(-News.clicks).paginate(page=int(page), per_page=int(per_page), error_out=False)
+    else:
+        cid += 1  # 由于测试数据分类中从0开始，而数据库中是从1开始的，所以用户点击的1实际上是2
+        paginate = db.session.query(News).filter(News.category_id == cid).paginate(page=int(page), per_page=int(per_page), error_out=False)
 
     # 3. 准备好要返回给浏览器的数据
     ret = {
