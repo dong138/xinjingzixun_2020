d01f1be835b90a12c224bdb899dd67ea7ac554f1
dong4716138 <dong4716138@163.com>
2020-09-22 20:36:10 +0800

    40-实现了注册功能

--- views/passport.py
+++ views/passport.py
@@ -1,5 +1,8 @@
 from flask import request
 
+from models import db
+from models.index import User
+
 from . import passport_blu
 
 
@@ -14,4 +17,24 @@ def register():
     # 2. 测试数据
     print(mobile, password, image_code, smscode)
 
-    return "成功"
+    # 2. 创建一个新的用户
+    # 2.1 先查询是否有这个相同的用户
+    if db.session.query(User).filter().first():
+        return "已经注册"
+
+    # 2.2 注册用户
+    # 将新用户的数据插入到数据库
+    user = User()
+    user.nick_name = mobile
+    user.password_hash = password  # 在第2版中会进行更改，到时会变成加密的
+    user.mobile = mobile
+    try:
+        db.session.add(user)
+        db.session.commit()
+        ret = "注册成功..."
+    except Exception as ret:
+        print("---->", ret)
+        db.session.rollback()  # 如果在将用户的信
+        ret = "注册失败..."
+
+    return ret
