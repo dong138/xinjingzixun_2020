0b02b9b1f3a3b24e0fe522702eb4f8674b2263e7
dong4716138 <dong4716138@163.com>
2020-09-22 20:14:45 +0800

    35-新建一个视图文件passport.py，且在__init__.py中创建一个新的蓝图

--- views/__init__.py
+++ views/__init__.py
@@ -1,5 +1,7 @@
 from flask import Blueprint
 
 index_blu = Blueprint("index_blu", __name__)
+passport_blu = Blueprint("passport_blu", __name__)
 
 from . import index
+from . import passport
---
+++ views/passport.py
@@ -0,0 +1,3 @@
+from . import passport_blu
+
+# 这里存放用passport_blu蓝图装饰的视图函数
