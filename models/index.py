5a4c695a647c6e170f0ca4ed66b2162a92f1377d
dong4716138 <dong4716138@163.com>
2020-09-22 20:25:46 +0800

    39-为了能够实现接下来的注册功能，肯定需要在数据库中添加一个与用户相关的数据表，所以我们在/models/index.py中添加User类模型。注意：添加之后 需要进行数据库迁移

--- models/index.py
+++ models/index.py
@@ -46,3 +46,25 @@ class Category(db.Model):
     name = db.Column(db.String(64), nullable=False)  # 分类名
     create_time = db.Column(db.DateTime, default=datetime.now)  # 记录的创建时间
     update_time = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)  # 记录的更新时间
+
+
+class User(db.Model):
+    """用户"""
+    __tablename__ = "user"
+
+    id = db.Column(db.Integer, primary_key=True)  # 用户编号
+    nick_name = db.Column(db.String(32), unique=True, nullable=False)  # 用户昵称
+    password_hash = db.Column(db.String(128), nullable=False)  # 加密的密码
+    mobile = db.Column(db.String(11), unique=True, nullable=False)  # 手机号
+    avatar_url = db.Column(db.String(256))  # 用户头像路径
+    create_time = db.Column(db.DateTime, default=datetime.now)  # 注册时间
+    last_login = db.Column(db.DateTime, default=datetime.now)  # 最后一次登录时间
+    is_admin = db.Column(db.Boolean, default=False)
+    signature = db.Column(db.String(512))  # 用户签名
+    gender = db.Column(  # 性别
+        db.Enum(
+            "MAN",  # 男
+            "WOMAN"  # 女
+        ),
+        default="MAN"
+    )
