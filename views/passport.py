edaf74638434a3f4260aea2c3e7cf0f178d4f049
dong4716138 <dong4716138@163.com>
2020-09-22 20:21:12 +0800

    38-获取前端传递的数据

--- views/passport.py
+++ views/passport.py
@@ -1,6 +1,17 @@
+from flask import request
+
 from . import passport_blu
 
 
 @passport_blu.route("/passport/register", methods=["GET", "POST"])
 def register():
+    # 1. 提取数据
+    mobile = request.json.get("mobile")
+    password = request.json.get("password")
+    image_code = request.json.get("image_code")
+    smscode = request.json.get("smscode")
+
+    # 2. 测试数据
+    print(mobile, password, image_code, smscode)
+
     return "成功"
