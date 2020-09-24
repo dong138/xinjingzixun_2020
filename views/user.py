348c3aff6177778d3dc37dbc8fa1dda2fc2e67a2
dong4716138 <dong4716138@163.com>
2020-09-24 21:48:44 +0800

    67-测试返回数据格式是否符合前端要求。成功

--- views/user.py
+++ views/user.py
@@ -1,1 +1,13 @@
+from flask import jsonify
+
 from . import user_blu
+
+
+@user_blu.route("/user/follow", methods=["POST"])
+def follow():
+    ret = {
+        "errno": 0,
+        "errmsg": "关注成功"
+    }
+
+    return jsonify(ret)
