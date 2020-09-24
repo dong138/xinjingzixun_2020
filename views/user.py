05646d16ea879c8d00e44f9d564f8bfcf3fc8440
dong4716138 <dong4716138@163.com>
2020-09-24 22:14:52 +0800

    74-根据action的不同实现"关注"、"取消关注"

--- views/user.py
+++ views/user.py
@@ -7,43 +7,55 @@ from . import user_blu
 
 @user_blu.route("/user/follow", methods=["POST"])
 def follow():
-    # 实现关注的流程
-    # 1. 提取当前作者的id
-    # 2. 提取当前登录用户的id
-    # 3. 判断之前是否已经关注过
-    # 4. 如果未关注，则进行关注
+    action = request.json.get("action")
 
-    # 1. 提取当前作者的id
-    news_author_id = request.json.get("user_id")
+    if action == "do":
+        # 实现关注的流程
+        # 1. 提取当前作者的id
+        # 2. 提取当前登录用户的id
+        # 3. 判断之前是否已经关注过
+        # 4. 如果未关注，则进行关注
 
-    # 2. 提取当前登录用户的id
-    user_id = session.get("user_id")
+        # 1. 提取当前作者的id
+        news_author_id = request.json.get("user_id")
 
-    # 3. 判断之前是否已经关注过
-    news_author = db.session.query(User).filter(User.id == news_author_id).first()
-    if user_id in [x.id for x in news_author.followers]:
-        return jsonify({
-            "errno": 3001,
-            "errmsg": "已经关注了，请勿重复关注..."
-        })
+        # 2. 提取当前登录用户的id
+        user_id = session.get("user_id")
 
-    # 4. 如果未关注，则进行关注
-    try:
-        follow = Follow(followed_id=news_author_id, follower_id=user_id)
-        db.session.add(follow)
-        db.session.commit()
+        # 3. 判断之前是否已经关注过
+        news_author = db.session.query(User).filter(User.id == news_author_id).first()
+        if user_id in [x.id for x in news_author.followers]:
+            return jsonify({
+                "errno": 3001,
+                "errmsg": "已经关注了，请勿重复关注..."
+            })
 
+        # 4. 如果未关注，则进行关注
+        try:
+            follow = Follow(followed_id=news_author_id, follower_id=user_id)
+            db.session.add(follow)
+            db.session.commit()
+
+            ret = {
+                "errno": 0,
+                "errmsg": "关注成功"
+            }
+
+            return jsonify(ret)
+
+        except Exception as ret:
+            ret = {
+                "errno": 3003,
+                "errmsg": "关注失败..."
+            }
+
+            return jsonify(ret)
+
+    else:
+        # 取消关注
         ret = {
-            "errno": 0,
-            "errmsg": "关注成功"
-        }
-
-        return jsonify(ret)
-
-    except Exception as ret:
-        ret = {
-            "errno": 3003,
-            "errmsg": "关注失败..."
+            "error": 0,
+            "errmsg": "取消关注成功"
         }
 
         return jsonify(ret)
