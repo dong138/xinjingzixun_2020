f40e4344e0651e02c8998cfb30bc2ada31fd5c74
dong4716138 <dong4716138@163.com>
2020-09-24 22:06:16 +0800

    70-关注成功

--- views/user.py
+++ views/user.py
@@ -1,5 +1,7 @@
-from flask import jsonify, session
+from flask import jsonify, session, request
 
+from models import db
+from models.index import User, Follow
 from . import user_blu
 
 
@@ -11,10 +13,37 @@ def follow():
     # 3. 判断之前是否已经关注过
     # 4. 如果未关注，则进行关注
 
+    # 1. 提取当前作者的id
+    news_author_id = request.json.get("user_id")
 
-    ret = {
-        "errno": 0,
-        "errmsg": "关注成功"
-    }
+    # 2. 提取当前登录用户的id
+    user_id = session.get("user_id")
 
-    return jsonify(ret)
+    # 3. 判断之前是否已经关注过
+    news_author = db.session.query(User).filter(User.id == news_author_id).first()
+    if user_id in [x.id for x in news_author.followers]:
+        return jsonify({
+            "errno": 3001,
+            "errmsg": "已经关注了，请勿重复关注..."
+        })
+
+    # 4. 如果未关注，则进行关注
+    try:
+        follow = Follow(followed_id=news_author_id, follower_id=user_id)
+        db.session.add(follow)
+        db.session.commit()
+
+        ret = {
+            "errno": 0,
+            "errmsg": "关注成功"
+        }
+
+        return jsonify(ret)
+
+    except Exception as ret:
+        ret = {
+            "errno": 3003,
+            "errmsg": "关注失败..."
+        }
+
+        return jsonify(ret)
