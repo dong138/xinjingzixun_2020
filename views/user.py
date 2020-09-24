5a5030030cd4d5cb5ff4cee92f1add26c975601e
dong4716138 <dong4716138@163.com>
2020-09-24 21:53:35 +0800

    68-关注的大体流程

--- views/user.py
+++ views/user.py
@@ -1,10 +1,17 @@
-from flask import jsonify
+from flask import jsonify, session
 
 from . import user_blu
 
 
 @user_blu.route("/user/follow", methods=["POST"])
 def follow():
+    # 实现关注的流程
+    # 1. 提取当前作者的id
+    # 2. 提取当前登录用户的id
+    # 3. 判断之前是否已经关注过
+    # 4. 如果未关注，则进行关注
+
+
     ret = {
         "errno": 0,
         "errmsg": "关注成功"
