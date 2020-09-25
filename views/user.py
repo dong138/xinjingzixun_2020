6afff82fa503c8cbbe4eddb0f57ea57db0de332b
dong4716138 <dong4716138@163.com>
2020-09-25 11:14:27 +0800

    17-实现取消关注

--- views/user.py
+++ views/user.py
@@ -7,23 +7,38 @@ from . import user_blu
 
 @user_blu.route("/user/follow", methods=["POST"])
 def follow():
-    # 实现关注的大体逻辑
-    # 从用户提交的数据中取出作者的id
+    # 提取这个请求的目的是，关注还是取消关注
     action = request.json.get("action")
+    # 从用户提交的数据中取出作者的id
     news_author_id = request.json.get("user_id")  # 作者的id
-
     # 从session中取出当前用户的id
     user_id = session.get("user_id")  # 当前用户的id
 
-    # 创建SQLAlchemy对象
-    follow = Follow(followed_id=news_author_id, follower_id=user_id)
+    if action == "do":
+        # 实现关注的大体逻辑
 
-    # 最终像follow数据表中添加一行记录
-    db.session.add(follow)
-    db.session.commit()
+        # 创建SQLAlchemy对象
+        follow = Follow(followed_id=news_author_id, follower_id=user_id)
 
-    ret = {
-        "errno": 0,
-        "errmsg": "关注成功..."
-    }
-    return jsonify(ret)
+        # 最终像follow数据表中添加一行记录
+        db.session.add(follow)
+        db.session.commit()
+
+        ret = {
+            "errno": 0,
+            "errmsg": "关注成功..."
+        }
+        return jsonify(ret)
+
+    elif action == "undo":
+
+        follow = db.session.query(Follow).filter(Follow.followed_id == news_author_id, Follow.follower_id == user_id).first()
+        db.session.delete(follow)
+        db.session.commit()
+
+        ret = {
+            "errno": 0,
+            "errmsg": "取消关注成功"
+        }
+
+        return jsonify(ret)
