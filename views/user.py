32a8beb16c14bfbca3f0e17a3ee1c7e655dc5f93
dong4716138 <dong4716138@163.com>
2020-09-25 18:10:32 +0800

    debug-2:用户未登录禁止访问用户中心页面

--- views/user.py
+++ views/user.py
@@ -1,4 +1,4 @@
-from flask import jsonify, session, request, render_template
+from flask import jsonify, session, request, render_template, redirect, url_for
 
 from models import db
 from models.index import User, Follow
@@ -81,6 +81,11 @@ def follow():
 @user_blu.route("/user/center")
 def user_center():
     nick_name = session.get("nick_name")
+
+    # 如果用户未登录，禁止访问用户中心
+    if not nick_name:
+        return redirect(url_for('index_blu.index'))
+
     return render_template("user.html", nick_name=nick_name)
 
 
