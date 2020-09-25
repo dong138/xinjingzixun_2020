f55fa0166e25add898d9baa50db867b604f4a44b
dong4716138 <dong4716138@163.com>
2020-09-25 16:56:28 +0800

    82-用新的用户中心相关js文件，替代/static/index/js中的部分js文件。下载地址：http://19python.howdoit.cn/98-download/更新部分js文件.zip

--- static/index/js/user_base_info.js
+++ static/index/js/user_base_info.js
@@ -6,20 +6,51 @@ function getCookie(name) {
 $(function () {
 
     $(".base_info").submit(function (e) {
-        e.preventDefault()
+        // 阻止表单默认提交行为
+        e.preventDefault();
 
-        var signature = $("#signature").val()
-        var nick_name = $("#nick_name").val()
-        var gender = $(".gender").val()
+        var signature = $("#signature").val();
+        var nick_name = $("#nick_name").val();
+        // 获取用户选择的性别
+        var gender = $(".gender:checked").val();
 
         if (!nick_name) {
-            alert('请输入昵称')
+            alert('请输入昵称');
             return
         }
         if (!gender) {
-            alert('请选择性别')
+            alert('请选择性别');
         }
 
-        // TODO 修改用户信息接口
+        // 组织参数
+        var params = {
+            "signature": signature,
+            "nick_name": nick_name,
+            "gender": gender
+        };
+        
+        // TODO 请求修改用户基本信息
+        $.ajax({
+            url: "/user/basic",
+            type: "post",
+            data: JSON.stringify(params),
+            contentType: "application/json",
+            headers: {
+                "X-CSRFToken": getCookie("csrf_token")
+            },
+            success: function (resp) {
+                if (resp.errno == "0") {
+                    // `设置`个人信息成功
+                    // 更新父窗口内容
+                    $('.user_center_name', parent.document).html(nick_name);
+                    $('#nick_name', parent.document).html(nick_name);
+                    $('.input_sub').blur()
+                }
+                else {
+                    // `设置`个人信息失败
+                    alert(resp.errmsg);
+                }
+            }
+        })
     })
-})
+});
