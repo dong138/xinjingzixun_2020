548ae171da527807e1fc11877dd71afc158db11e
dong4716138 <dong4716138@163.com>
2020-10-04 21:27:10 +0800

    106-可以点击第几页的关注了

--- views/user.py
+++ views/user.py
@@ -220,9 +220,12 @@ def user_avatar():
 
 @user_blu.route("/user/user_follow.html")
 def user_follow():
+    # 获取当前需要展示的页数
+    page = int(request.args.get("page", 1))
+
     user_id = session.get("user_id")
     user = db.session.query(User).filter(User.id == user_id).first()
 
-    paginate = user.followers.paginate(1, 2, False)
+    paginate = user.followers.paginate(page, 2, False)
 
     return render_template("user_follow.html", paginate=paginate)
