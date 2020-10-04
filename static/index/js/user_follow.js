995348640adfabdffd50255e18f2aec2c83f3c19
dong4716138 <dong4716138@163.com>
2020-10-04 20:15:07 +0800

    18-用户中心：基本资料、密码

--- static/index/js/user_follow.js
+++ static/index/js/user_follow.js
@@ -6,7 +6,37 @@ function getCookie(name) {
 
 $(function () {
 
-    $(".focused").click(function () {
-        // TODO 取消关注当前新闻作者
+  $(".focused").click(function () {
+        // 获取参数
+        var user_id = $(this).attr("data-user-id");
+        var action = "undo";
+
+        // 组织参数
+        var params = {
+            "user_id": user_id,
+            "action": action
+        };
+
+        // TODO 取消关注当前用户
+        $.ajax({
+            url: "/user/follow",
+            type: "post",
+            data: JSON.stringify(params),
+            contentType: "application/json",
+            headers: {
+                "X-CSRFToken": getCookie("csrf_token")
+            },
+            success: function (resp) {
+                if (resp.errno == "0") {
+                    // `取消关注`成功
+                    // 刷新当前页面
+                    location.reload();
+                }
+                else {
+                    // `取消关注`成功
+                    alert(resp.errmsg);
+                }
+            }
+        })
     })
-})
+});
--- views/user.py
+++ views/user.py
@@ -1,7 +1,7 @@
-from flask import jsonify, request, session
+from flask import jsonify, request, session, render_template
 
 from models import db
-from models.index import Follow
+from models.index import Follow, User
 from . import user_blu
 
 
@@ -42,3 +42,90 @@ def follow():
         }
 
         return jsonify(ret)
+
+
+@user_blu.route("/user/user_center")
+def user_center():
+    return render_template("user.html")
+
+
+@user_blu.route("/user/user_base_info.html")
+def user_base_info():
+    return render_template("user_base_info.html")
+
+
+@user_blu.route("/user/basic", methods=["POST"])
+def user_basic():
+    # 1. 取参数
+    nick_name = request.json.get("nick_name")
+    signature = request.json.get("signature")
+    gender = request.json.get("gender")
+
+    # 2. 查询用户
+    user_id = session.get("user_id")
+    user = db.session.query(User).filter(User.id == user_id).first()
+    if user:
+        # 有这个用户
+        # 3.修改用户数据
+        user.nick_name = nick_name
+        user.signature = signature
+        user.gender = gender
+        # 4. 保存数据
+        db.session.commit()
+        ret = {
+            "errno": 0,
+            "errmsg": "成功"
+        }
+    else:
+        # 没有这个用户
+        ret = {
+            "errno": 4001,
+            "errmsg": "修改失败"
+        }
+
+    # 5. 返回操作结果信息
+    return jsonify(ret)
+
+
+@user_blu.route("/user/user_pass_info.html")
+def user_pass_info():
+    return render_template("user_pass_info.html")
+
+
+@user_blu.route("/user/password", methods=["POST"])
+def user_password():
+    # 1. 取数据
+    # old_password = request.json.get("old_password")
+    new_password = request.json.get("new_password")
+
+    print("--------1--------")
+
+    # 2. 查询用户
+    user_id = session.get("user_id")
+    user = db.session.query(User).filter(User.id == user_id).first()
+    if user:
+        # 有这个用户
+        # 3.修改用户数据
+        print("--------2--------")
+        print(user)
+        print(new_password)
+        user.password_hash = new_password
+        print("--------2-0-------")
+        # 4. 保存数据
+        db.session.commit()
+        print("--------2-1-------")
+        ret = {
+            "errno": 0,
+            "errmsg": "成功"
+        }
+    else:
+        # 没有这个用户
+        ret = {
+            "errno": 4002,
+            "errmsg": "修改失败"
+        }
+
+    print("--------3--------")
+
+    # 5. 返回对应数据
+    return jsonify(ret)
