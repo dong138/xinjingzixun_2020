b60572ed8483e54a2d5b2469d835a82b2830fea6
dong4716138 <dong4716138@163.com>
2020-10-05 20:37:23 +0800

    113-将上传图片代码进行封装，得到一个函数，这样便于在视图函数中调用

--- utils/image_qiniu/__init__.py
+++ utils/image_qiniu/__init__.py
@@ -14,16 +14,30 @@ q = Auth(access_key, secret_key)
 # 要上传的空间
 bucket_name = 'xinjingzixun2020'
 
-# 上传后保存的文件名
-key = 'my-python-logo.png'
 
-# 生成上传 Token，可以指定过期时间等
-token = q.upload_token(bucket_name, key, 3600)
+def upload_image_to_qiniu(localfile, key):
+    """
+    上传图片到七牛云
+    :param localfile:要上传的文件路径
+    :param key:保存到七牛云之后的文件名
+    :return: True表示成功
+    """
+    # 上传后保存的文件名
+    # key = 'my-python-logo.png'
 
-# 要上传文件的本地路径
-localfile = './bbb.png'
+    # 生成上传 Token，可以指定过期时间等
+    token = q.upload_token(bucket_name, key, 3600)
 
-ret, info = put_file(token, key, localfile)
-print(info)
-assert ret['key'] == key
-assert ret['hash'] == etag(localfile)
+    # 要上传文件的本地路径
+    # localfile = './bbb.png'
+
+    ret, info = put_file(token, key, localfile)
+    print(info)
+    assert ret['key'] == key
+    assert ret['hash'] == etag(localfile)
+
+    return True
+
+
+if __name__ == "__main__":
+    upload_image_to_qiniu()
