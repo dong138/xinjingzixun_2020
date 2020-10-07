9ef0f0a056520aad115d3d7daf1fcdc08c493e67
dong4716138 <dong4716138@163.com>
2020-10-07 22:35:11 +0800

    139-"用户中心"-"新闻列表"页中，看到新闻的正确信息

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask_script import Manager
 
 from views import index_blu, passport_blu, user_blu, news_blu
 from models import db
-from utils.common import show_top_6_news_style
+from utils.common import show_top_6_news_style, show_news_status_name, show_news_status_style_name
 
 # 创建flask应用对象
 app = Flask(__name__)
@@ -23,6 +23,8 @@ db.init_app(app)
 
 # 添加过滤器
 app.add_template_filter(show_top_6_news_style)
+app.add_template_filter(show_news_status_name)
+app.add_template_filter(show_news_status_style_name)
 
 # 添加数据库迁移等工具
 manager = Manager(app)
--- utils/common.py
+++ utils/common.py
@@ -6,3 +6,23 @@ def show_top_6_news_style(index):
     elif index == 3:
         return "third"
     return ""
+
+
+def show_news_status_name(index):
+    if index == 0:
+        return "已通过"
+    elif index == 1:
+        return "审核中"
+    elif index == -1:
+        return "未通过"
+    return ""
+
+
+def show_news_status_style_name(index):
+    if index == 0:
+        return "pass"
+    elif index == 1:
+        return "review"
+    elif index == -1:
+        return "nopass"
+    return ""
