853232147bf94f37c568a97cf12af5643ec04ebd
dong4716138 <dong4716138@163.com>
2020-10-07 22:09:57 +0800

    134-基本实现"新闻发布"的后端逻辑

--- views/user.py
+++ views/user.py
@@ -4,7 +4,7 @@ import time
 from flask import jsonify, session, request, render_template, redirect, url_for
 
 from models import db
-from models.index import User, Follow, Category
+from models.index import User, Follow, Category, News
 from utils.image_qiniu import upload_image_to_qiniu
 from . import user_blu
 
@@ -263,6 +263,18 @@ def new_release():
     f = request.files.get("index_image")
 
     print(title, category, digest, content, f)
+
+    news = News()
+    news.title = title
+    news.category_id = category
+    news.source = "个人发布"
+    news.digest = digest
+    news.content = content
+    news.user_id = session.get("user_id")
+    news.status = 1  # 1代表 正在审核
+
+    db.session.add(news)
+    db.session.commit()
 
     ret = {
         "errno": 0,
