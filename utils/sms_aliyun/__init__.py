9de859a143886828c2c4560eeced3a5048722b01
dong4716138 <dong4716138@163.com>
2020-10-11 20:27:57 +0800

    160-【发送短信验证码】将阿里云发送短信demo，封装为函数，这样便于调用

--- utils/sms_aliyun/__init__.py
+++ utils/sms_aliyun/__init__.py
@@ -4,22 +4,34 @@
 from aliyunsdkcore.client import AcsClient
 from aliyunsdkcore.request import CommonRequest
 
-client = AcsClient('LTAI4GJcCBqAEs63nNVNsJ63', 'hZXTnGJvkT54E6bqYH4jwAJO5Tc9Mx', 'cn-hangzhou')
 
-request = CommonRequest()
-request.set_accept_format('json')
-request.set_domain('dysmsapi.aliyuncs.com')
-request.set_method('POST')
-request.set_protocol_type('https')  # https | http
-request.set_version('2017-05-25')
-request.set_action_name('SendSms')
+def send_msg_to_phone(phone_num, content):
+    """
+    将阿里云提供的发送短信demo，封装为函数，这样使用时更加方便
+    :param phone_num:
+    :param content:
+    :return:
+    """
+    client = AcsClient('LTAI4GJcCBqAEs63nNVNsJ63', 'hZXTnGJvkT54E6bqYH4jwAJO5Tc9Mx', 'cn-hangzhou')
 
-request.add_query_param('RegionId', "cn-hangzhou")
-request.add_query_param('PhoneNumbers', "13146060336")
-request.add_query_param('SignName', "dong4716138")
-request.add_query_param('TemplateCode', "SMS_167532197")
-request.add_query_param('TemplateParam', "{\"code\":\"112233\"}")
+    request = CommonRequest()
+    request.set_accept_format('json')
+    request.set_domain('dysmsapi.aliyuncs.com')
+    request.set_method('POST')
+    request.set_protocol_type('https')  # https | http
+    request.set_version('2017-05-25')
+    request.set_action_name('SendSms')
 
-response = client.do_action(request)
-# python2:  print(response)
-print(str(response, encoding='utf-8'))
+    request.add_query_param('RegionId', "cn-hangzhou")
+    request.add_query_param('PhoneNumbers', phone_num)
+    request.add_query_param('SignName', "dong4716138")
+    request.add_query_param('TemplateCode', "SMS_167532197")
+    request.add_query_param('TemplateParam', "{\"code\":\"%s\"}" % content)
+
+    response = client.do_action(request)
+    # python2:  print(response)
+    print(str(response, encoding='utf-8'))
+
+
+if __name__ == '__main__':
+    send_msg_to_phone("13146060336", "876542")
