77a997d4e6ec7bd8f56df523e15d7658efe3c0c8
dong4716138 <dong4716138@163.com>
2020-10-12 08:48:46 +0800

    173-【后台】用户列表 页可以展示

--- views/admin.py
+++ views/admin.py
@@ -11,3 +11,8 @@ def admin():
 @admin_blu.route("/admin/user_count.html")
 def user_count():
     return render_template("admin/user_count.html")
+
+
+@admin_blu.route("/admin/user_list.html")
+def user_list():
+    return render_template("admin/user_list.html")
