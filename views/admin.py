17cdaca80f2136a661930cde7ef71ffdd1c2fd70
dong4716138 <dong4716138@163.com>
2020-10-12 08:45:47 +0800

    172-【后台】用户统计页面可以展示

--- views/admin.py
+++ views/admin.py
@@ -6,3 +6,8 @@ from . import admin_blu
 @admin_blu.route("/admin")
 def admin():
     return render_template("admin/index.html")
+
+
+@admin_blu.route("/admin/user_count.html")
+def user_count():
+    return render_template("admin/user_count.html")
